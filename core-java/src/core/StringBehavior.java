49b7cd478ad097c57c3a75d9e0fe692e0c0bb22b
Chetan Singh1 <chetan.singh1@synapse.com>
2018-11-27 22:54:46 +0530

    custome blocking queue

--- core-java/src/core/StringBehavior.java
+++ core-java/src/core/StringBehavior.java
@@ -1,4 +1,4 @@
-
+package core;
 public class StringBehavior
 {
 	public static void main(String[] args) {
--- core-java/src/core/java8/completablefuture/TestCompletableFuture.java
+++ core-java/src/core/java8/completablefuture/TestCompletableFuture.java
@@ -1,4 +1,4 @@
-package com.cs.core.completablefuture;
+package core.java8.completablefuture;
 
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
